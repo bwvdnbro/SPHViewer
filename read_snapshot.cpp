0cc048862ec950cbe02c8feb1565d6412fe058af
Bert Vandenbroucke <bert.vandenbroucke@ugent.be>
2016-07-26 12:17:27 +0200

    Fixed small error in license headers

--- read_snapshot.cpp
+++ read_snapshot.cpp
@@ -2,18 +2,18 @@
  * This file is part of SPHViewer
  * Copyright (C) 2016 Bert Vandenbroucke (bert.vandenbroucke@gmail.com)
  *
- * Shadowfax is free software: you can redistribute it and/or modify
+ * SPHViewer is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Shadowfax is distributed in the hope that it will be useful,
+ * SPHViewer is distributed in the hope that it will be useful,
  * but WITOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with Shadowfax. If not, see <http://www.gnu.org/licenses/>.
+ * along with SPHViewer. If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
 
 #include <fstream>
